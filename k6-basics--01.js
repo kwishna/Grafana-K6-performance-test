2b4b927d1f9bda71cb8253e68e172a1f92a9ff14
kwishna <kwishna@gmail.com>
2024-05-20 02:21:24 +0530

    k6 scenarios hands-on

---
+++ k6-basics--01.js
@@ -0,0 +1,143 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+// import { Options } from "k6/options";
+
+/**
+ * @type {Options}
+ */
+export const options = {
+
+  scenarios: {
+    // "scenario_name_1": {
+    //  // A fixed amount of iterations are shared between a number of VUs.
+    //   executor: "shared-iterations",
+    //
+    //   // common scenario configuration
+    //   startTime: '1s', // Waiting time before execution starts.
+    //   gracefulStop: '5s', // Time to wait for iterations to finish executing before stopping them forcefully.
+    //   env: {
+    //     date: `${Date.now()}`
+    //   },
+    //   tags: {
+    //     tagName: '@GetReq'
+    //   },
+    //
+    //   exec: 'get_request', // Name of exported JS function to execute. Ex. 'default'
+    //
+    //   // executor-specific configuration. || 100 iterations shared among 10 VUs ||
+    //   vus: 10, // Number of VUs to run concurrently.
+    //   iterations: 100, // Number of iterations to execute across per VU.
+    //   maxDuration: '10s' // Maximum scenario duration before it's forcibly stopped (excluding gracefulStop).
+    // },
+    // ----------------------------------------------------------------------------------------------------------------------
+    // ======================================================================================================================
+    // ----------------------------------------------------------------------------------------------------------------------
+    // "scenario_name_2": {
+    //   // Each VU executes an exact number of iterations.
+    //   executor: "per-vu-iterations",
+    //
+    //   // common scenario configuration
+    //   startTime: '1s', // Waiting time before execution starts.
+    //   gracefulStop: '5s', // Time to wait for iterations to finish executing before stopping them forcefully.
+    //   env: {
+    //     date: `${Date.now()}`
+    //   },
+    //   tags: {
+    //     tagName: '@GetReq'
+    //   },
+    //
+    //   exec: 'get_request', // Name of exported JS function to execute. Ex. 'default'
+    //
+    //   // executor-specific configuration || 100 iterations for each of 10 VUs ||
+    //   vus: 10,
+    //   iterations: 100
+    // }
+    // ----------------------------------------------------------------------------------------------------------------------
+    // ======================================================================================================================
+    // ----------------------------------------------------------------------------------------------------------------------
+    // "scenario_name_3": {
+    //   // A fixed number of VUs execute as many iterations as possible for a specified amount of time.
+    //   executor: "constant-vus",
+    //
+    //   // common scenario configuration
+    //   startTime: '1s', // Waiting time before execution starts.
+    //   gracefulStop: '5s', // Time to wait for iterations to finish executing before stopping them forcefully.
+    //   env: {
+    //     date: `${Date.now()}`
+    //   },
+    //   tags: {
+    //     tagName: '@GetReq'
+    //   },
+    //
+    //   exec: 'get_request', // Name of exported JS function to execute. Ex. 'default'
+    //
+    //   // executor-specific configuration || 10 looping VUs for 15s ||
+    //   vus: 10,
+    //   duration: '15s' // Total scenario duration (excluding gracefulStop time)
+    // }
+    // ----------------------------------------------------------------------------------------------------------------------
+    // ======================================================================================================================
+    // ----------------------------------------------------------------------------------------------------------------------
+    // "scenario_name_4": {
+    //   // A variable number of VUs execute as many iterations as possible for a specified amount of time.
+    //   executor: "ramping-vus",
+    //   exec: 'get_request', // Name of exported JS function to execute. Ex. 'default'
+    //
+    //   // executor-specific configuration
+    //
+    //   /** Array of objects that specify the number of VUs to ramp up or down to. */
+    //   // Up to 30 looping VUs for 1m20s over 6 stages
+    //   stages: [
+    //     {
+    //       duration: '10s', // Stage duration
+    //       target: 10 // Target number of VUs.
+    //     },
+    //     {
+    //       duration: '20s', // Stage duration
+    //       target: 20 // Target number of VUs.
+    //     },
+    //     {
+    //       duration: '30s', // Stage duration
+    //       target: 30 // Target number of VUs.
+    //     },
+    //     {
+    //       duration: '10s', // Stage duration
+    //       target: 10 // Target number of VUs.
+    //     }
+    //     // Total 1 minutes 10 seconds approx.
+    //   ],
+    //   startVUs: 5, // Number of VUs to run at test start.
+    //   gracefulRampDown: '10s' // Time to wait for an already started iteration to finish before stopping it during a ramp down.
+    // },
+    // ----------------------------------------------------------------------------------------------------------------------
+    // ======================================================================================================================
+    // ----------------------------------------------------------------------------------------------------------------------
+    "scenario_name_5": {
+      // A fixed number of iterations are executed in a specified period of time.
+      executor: "constant-arrival-rate",
+
+      // common scenario configuration
+      startTime: '1s', // Waiting time before execution starts.
+      gracefulStop: '5s', // Time to wait for iterations to finish executing before stopping them forcefully.
+      env: {
+        date: `${Date.now()}`
+      },
+      tags: {
+        tagName: '@GetReq'
+      },
+
+      exec: 'get_request', // Name of exported JS function to execute. Ex. 'default'
+
+      // executor-specific configuration ||  ||
+      duration: '20s', // Total scenario duration (excluding `gracefulStop`)
+      rate: 10, // Number of iterations to execute each `timeUnit` period.
+      timeUnit: '5s', // Period of time to apply the `rate` value.
+      preAllocatedVUs: 5, // Number of VUs to pre-allocate before test start in order to preserve runtime resources.
+      maxVUs: 15 // Maximum number of VUs to allow during the test run.
+    }
+  },
+};
+
+export function get_request() {
+  http.get("http://test.k6.io");
+}
