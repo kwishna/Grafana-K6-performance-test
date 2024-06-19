3e457b00e3ef9a9761bbaa97b37a3fe3a7474731
kwishna <kwishna@gmail.com>
2024-06-20 03:23:16 +0530

    shared iteration

--- k6-basics--01.ts
+++ k6-basics--01.ts
@@ -1,17 +1,28 @@
 import http from "k6/http";
+//@ts-ignore
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 import { check, sleep } from "k6";
 import { Options } from "k6/options";
 
 export const options: Options = {
 
   scenarios: {
-      "scenario_name": {
-        executor: "shared-iterations",
-        startTime: '2s', // Waiting time before execution starts.
-        gracefulStop: '2s',
-        vus: 2,
-        iterations: 10
+    // "scenario_name": {
+    //   executor: "shared-iterations",
+    //   startTime: '2s', // Waiting time before execution starts.
+    //   gracefulStop: '2s',
+    //   vus: 2,
+    //   iterations: 10
+    // },
+    browser: {
+      executor: 'shared-iterations',
+      options: {
+        browser: {
+          type: 'chromium',
+          headless: false
+        },
       }
+    },
   },
 };
 
@@ -21,4 +32,12 @@ export default function () {
   // Validate response status
   check(res, { "status was 200": (r) => r.status == 200 });
   sleep(1);
+}
+
+export function handleSummary(data) {
+  data
+  return {
+    "result.html": htmlReport(data),
+    "summary.json": JSON.stringify(data)
+  };
 }
