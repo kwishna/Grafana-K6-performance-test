6e15af3450909eeae1779bb3533148aea62c3975
kwishna <kwishna@gmail.com>
2024-08-22 23:32:03 +0530

    browser

---
+++ src/course-tutorial/code--007-browser.js
@@ -0,0 +1,49 @@
+import { check } from 'k6';
+import { browser } from 'k6/experimental/browser';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+
+export const options = {
+  scenarios: {
+    ui: {
+      executor: 'shared-iterations',
+      options: {
+        browser: {
+          type: 'chromium',
+        },
+      },
+    },
+  },
+  thresholds: {
+    checks: ["rate==1.0"]
+  }
+}
+
+export default async function () {
+
+  const page = browser.newPage();
+
+  try {
+    await page.goto('https://test.k6.io/my_messages.php');
+
+    page.locator('input[name="login"]').type('admin');
+    page.locator('input[name="password"]').type('123');
+
+    const submitButton = page.locator('input[type="submit"]');
+
+    await Promise.all([page.waitForNavigation(), submitButton.click()]);
+
+    check(page, {
+      'header': p => p.locator('h2').textContent() == 'Welcome, admin!',
+    });
+  } finally {
+    page.close();
+  }
+}
+
+export function handleSummary(data) {
+  return {
+    "result.html": htmlReport(data),
+    stdout: textSummary(data, { indent: " ", enableColors: true }),
+  };
+}
